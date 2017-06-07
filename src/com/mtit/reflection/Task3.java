a9332f3644a8661773aa37a3fc0e4860fa6ecf21
Rishanthakumar Rasarathinam <rishanthakumarr@zone24x7.com>
2017-06-07 17:11:59 +0530

    Task3 completed

--- src/com/mtit/reflection/Task3.java
+++ src/com/mtit/reflection/Task3.java
@@ -1,5 +1,9 @@
 package com.mtit.reflection;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
+
 /**
  * Class representing the Task3
  */
@@ -7,5 +11,32 @@ public class Task3 {
 
     public static void main(String argd[]) {
 
+        try {
+            Class<?> clazz = Class.forName("com.mtit.test_classes.Employee");
+
+            // Get all methods of a class.
+            Method[] methods = clazz.getDeclaredMethods();
+
+            for(int i=0; i<methods.length; i++) {
+                System.out.println(Modifier.toString(methods[i].getModifiers()));
+                System.out.print("|| Return type => " + methods[i].getReturnType());
+                System.out.print("|| Method Name => " + methods[i].getName());
+
+                if (methods[i].getParameterCount() != 0) {
+                    System.out.print("|| Method Parameter => ");
+
+                    Parameter[] parameters = methods[i].getParameters();
+
+                    for (int z=0;z<parameters.length;z++) {
+                        System.out.print(parameters[z].getType().getSimpleName() + " ");
+                    }
+                }
+                System.out.println();
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
+
